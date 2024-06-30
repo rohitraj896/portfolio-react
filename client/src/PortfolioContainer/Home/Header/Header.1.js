8f18fe133e5bb78a5b11df18d58a5c7f96499964
Rohit Kumar <rohitkr3390@gmail.com>
2024-06-30 16:24:13 +0530

    header

--- client/src/PortfolioContainer/Home/Header/Header.1.js
+++ client/src/PortfolioContainer/Home/Header/Header.1.js
@@ -1,13 +1,11 @@
 import React, { useState } from "react";
-import "./Header.css";
-
-import { TOTAL_SCREEN, GET_SCREEN_INDEX } from "../../../utilities/commonUtils";
-
+import {
+  TOTAL_SCREENS,
+  GET_SCREEN_INDEX,
+} from "../../../utilities/commonUtils";
 import ScrollService from "../../../utilities/ScrollService";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Header = () => {
+export const Header = () => {
   const [selectedScreen, setSelectedScreen] = useState(0);
   const [showHeaderOptions, setShowHeaderOptions] = useState(false);
 
@@ -20,7 +18,7 @@ const Header = () => {
     ScrollService.currentScreenBroadCaster.subscribe(updateCurrentScreen);
 
   const getHeaderOptions = () => {
-    return TOTAL_SCREEN.map((screen, i) => (
+    return TOTAL_SCREENS.map((screen, i) => (
       <div
         key={screen.screen_name}
         className={getHeaderOptionsClass(i)}
@@ -33,7 +31,7 @@ const Header = () => {
 
   const getHeaderOptionsClass = index => {
     let classes = "header-option";
-    if (index < TOTAL_SCREEN.length - 1) classes += "header-option-seperator";
+    if (index < TOTAL_SCREENS.length - 1) classes += "header-option-seperator";
 
     if (selectedScreen === index) classes += "selected-header-option";
     return;
@@ -49,35 +47,5 @@ const Header = () => {
     setShowHeaderOptions(false);
   };
 
-  return (
-    <div>
-      <div
-        className="header-container"
-        onClick={() => setShowHeaderOptions(!showHeaderOptions)}
-      >
-        <div className="header-parent">
-          <div
-            className="header-hamburger"
-            onClick={() => setShowHeaderOptions(!showHeaderOptions)}
-          >
-            <FontAwesomeIcon className="header-hamburger-bars" icon={faBars} />
-          </div>
-          <div className="header-logo">
-            <span>Rohit</span>
-          </div>
-          <div
-            className={
-              showHeaderOptions
-                ? "header-options show-hamburger-options"
-                : "header-options"
-            }
-          >
-            {getHeaderOptions()}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
-
-export default Header;
